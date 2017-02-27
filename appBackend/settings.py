da08d52802044252c2af7eb1d062dc35ba6691fe
Jim Notaro <footballmoose797@yahoo.com>
2017-02-27 02:21:33 -0500

    Commit through pycharm

--- appBackend/settings.py
+++ appBackend/settings.py
@@ -27,9 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     "192.168.5.1",
-    "ec2-54-200-169-163.us-west-2.compute.amazonaws.com/",
-    "Not a host"
-]
+    "ec2-54-200-169-163.us-west-2.compute.amazonaws.com",
+    ]
 
 
 # Application definition
