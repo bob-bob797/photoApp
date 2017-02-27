7a912dc153b006a953b04c0ba3ecf9b2103aae1b
Jim Notaro <footballmoose797@yahoo.com>
2017-02-27 02:53:53 -0500

    Including views

--- photoApp/views.py
+++ photoApp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from PIL import Image
 
 def index(request):
-    return HttpResponse("Hello, worlds")
+    return HttpResponse(Image.open("Dog.jpg"))
+
+def photoReturn(request):
+    return HttpResponse(Image.open("Dog.jpg"))
