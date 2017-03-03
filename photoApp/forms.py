1c0c6ab8ba53f2330e7a55714e6b679af421b476
footballmoose797@yahoo.com <footballmoose797@yahoo.com>
2017-03-03 05:46:31 -0500

    Post working

--- photoApp/forms.py
+++ photoApp/forms.py
@@ -2,7 +2,8 @@ from django import forms
 
 
 class documentForm(forms.Form):
-    picture = forms.FileField()
+    #picture = forms.FileField()
+    picture = forms.CharField()
     name = forms.CharField()
     room_code = forms.CharField()
 
--- photoApp/webViews.py
+++ photoApp/webViews.py
@@ -1,3 +1,5 @@
+import base64
+
 from django.http import HttpResponse
 from rest_framework import generics
 
@@ -19,30 +21,15 @@ class DisplayRoom(generics.ListAPIView):
     def get_queryset(self):
         return room.objects.filter(room_code=self.kwargs["room_code"])
 
-class displayPicture(generics.RetrieveAPIView):
-    serializer_class = pictureSerializer
-    queryset = picture.objects.all()
-
-    def get_object(self):
-        return picture.objects.all().first()
-
-def postPicture(request):
-    if request.method == ' POST':
-        print request.FILES['file']
-    return HttpResponse("Thanks")
-
-class PostPicture(generics.CreateAPIView):
-    serializer_class = pictureSerializer
-
-    def get_queryset(self):
-        return room.objects.filter(room_code=self.kwargs["room_code"])
-
-@
 def simple_upload(request):
     if request.method == 'POST':
         form = documentForm(request.POST, request.FILES)
         if form.is_valid():
             this_room = room.objects.filter(room_code=form.data["room_code"]).first()
-            pic = picture(pic=request.FILES['picture'], name=form.data["name"], room=this_room)
-            pic.save()
+            pic = base64.b64decode(form.data['picture'])
+            with open(form.data['name']+'.jpg', 'wb+') as f:
+                f.write(pic)
+                pic = picture(name=form.data["name"] + '.jpg', room=this_room)
+                pic.pic.save(form.data['name']+'.jpg', f)
+                pic.save()
             return HttpResponse("Received")
